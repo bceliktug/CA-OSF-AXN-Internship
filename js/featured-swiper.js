2c4aa13b4a08d04dab74b96c074e7be85d65c891
bceliktug <bulutcan.celiktug@gmail.com>
2022-07-25 16:26:25 +0300

    complated home page with some bugs to fix

---
+++ js/featured-swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    enable: true,
+    delay: 5000,
+  }
+
+});
